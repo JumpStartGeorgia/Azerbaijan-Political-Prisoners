338fe26c06a54b41fd8924ba0843e3d3bd926925
nwshane <django09@gmail.com>
2014-09-15 12:54:53 +0400

    parser.rb now outputs a csv file constructed out of a few rows manually entered into the parser.

--- parser/parser.rb
+++ parser/parser.rb
@@ -1,5 +1,23 @@
 require 'rubygems'
-require 'nokogiri'
+require 'Nokogiri'
+require 'csv'
 
-page = Nokogiri::HTML(open("../list.html"))   
-puts page.class   # => Nokogiri::HTML::Document
+list = Nokogiri::HTML(open( File.dirname(__FILE__) + '/../list.html' ))
+
+rows = []
+rows.push([ 'Nathan', 'Shane' ])
+rows.push([ 'James', 'Dean' ])
+rows.push([ 'Joe', 'Hobbit' ])
+rows.push([ 'John', 'Hobbit' ])
+
+CSV_FILE_PATH = File.join( File.dirname(__FILE__), '../output.csv' );
+
+CSV.open( CSV_FILE_PATH, 'wb' ) do |csv|
+  #Each row should have the first name of the person, followed by the last name
+  csv << ['first', 'last']
+  rows.each {
+    |row|
+    csv << row
+  }
+
+end

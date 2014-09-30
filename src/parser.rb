8232475d2d0bc3a47c620a2cd0614d68dbab32b2
nwshane <django09@gmail.com>
2014-09-30 13:31:02 +0400

    Removed unused cleanList.html.

--- src/parser.rb
+++ src/parser.rb
@@ -2,7 +2,6 @@ require 'Nokogiri'
 require 'csv'
 require_relative 'List.rb'
 
-
 #Create list object containing cleaned and structured HTML
 input_path = File.dirname(__FILE__) + '/../input/list.html'
 list = List.new(input_path)

b36175347ce4fc3984eeb38e32155740745a2485
nwshane <django09@gmail.com>
2014-11-06 11:26:13 +0400

    Migrating prisoner names from prisoner.csv to prisoner table in DB.

--- lib/aug2014DataToDb.rb
+++ lib/aug2014DataToDb.rb
@@ -1,7 +1,13 @@
+require 'csv'
+
 module Aug2014DataToDb
     def self.migratePrisoners
         Prisoner.destroy_all
 
-        Prisoner.create(name: 'Nathan')
+        CSV.foreach("#{Rails.root}/lib/aug2014PdfParser/output/prisoners.csv", "r") do |row|
+            if $. != 1
+                Prisoner.create(name: row[1])
+            end
+        end
     end
 end

6a5cd40240accda7feaac6efa8c2ff28ac7a535a
nwshane <django09@gmail.com>
2014-11-10 11:03:33 +0400

    Migrated initial subtypes data from spreadsheet to database and linked to parent types.

--- lib/aug2014DataToDb.rb
+++ lib/aug2014DataToDb.rb
@@ -11,6 +11,7 @@ module Aug2014DataToDb
                 Prisoner.create(name: row[1])
 
                 type = row[2]
+                #There is no spreadsheet of unique types, so we ensure here that the same type is not created twice
                 if !outputTypes.include? type
                     outputTypes.push(type)
                     Type.create(name: type)
@@ -32,6 +33,16 @@ module Aug2014DataToDb
                 Prison.create(name: row[0])
             end
         end
+
+        CSV.foreach("#{Rails.root}/lib/aug2014PdfParser/output/subtypes.csv", "r") do |row|
+            if $. != 1
+                Subtype.create(
+                    name: row[1],
+                    type: Type.where(name: row[2]).first,
+                    description: row[3]
+                )
+            end
+        end
     end
 
     def self.destroyData
@@ -39,5 +50,6 @@ module Aug2014DataToDb
         Charge.destroy_all
         Prison.destroy_all
         Type.destroy_all
+        Subtype.destroy_all
     end
 end

1f40cab6252a2829c5a3a6ee3e4445066c651330
nwshane <django09@gmail.com>
2014-11-11 11:52:40 +0400

    Added has_many :through relationship between articles and incidents through charges.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ActiveRecord::Base
   belongs_to :criminal_code
+  has_many :charges
+  has_many :incidents, through: :charges
 end
--- app/models/incident.rb
+++ app/models/incident.rb
@@ -3,4 +3,6 @@ class Incident < ActiveRecord::Base
   belongs_to :prison
   belongs_to :type
   belongs_to :subtype
+  has_many :charges
+  has_many :articles, through: :charges
 end

475a133a0f80ee0c05b6c113e1c8fba33cbdb448
nwshane <django09@gmail.com>
2014-11-11 13:29:27 +0400

    Removed create_charges_incidents migration because the table is no longer being used.

--- db/migrate/20141110073612_create_charges_incidents.rb
+++
@@ -1,8 +0,0 @@
-class CreateChargesIncidents < ActiveRecord::Migration
-  def change
-    create_table :charges_incidents, id: false do |t|
-        t.belongs_to :charge
-        t.belongs_to :incident
-    end
-  end
-end

ba14faddb562284e92af95068f9268c511fabea2
nwshane <django09@gmail.com>
2014-11-12 14:17:01 +0400

    Factored displayCharges function out of Incident Show and Index Views into charges_helper.rb.

--- app/helpers/charges_helper.rb
+++ app/helpers/charges_helper.rb
@@ -1,2 +1,11 @@
 module ChargesHelper
+    def displayCharges(charges)
+        chargesString = ''
+
+        charges.each do |charge|
+            chargesString = chargesString + (link_to charge.article.number, charge) + ' '
+        end
+
+        return chargesString.html_safe
+    end
 end

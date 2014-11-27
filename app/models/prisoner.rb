eee87be493429130ef0214a5687b9c1afd866255
nwshane <django09@gmail.com>
2014-11-27 12:38:24 +0400

    Now hiding incidents when they are marked_for_destruction.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,4 +1,13 @@
 class Prisoner < ActiveRecord::Base
-    has_many :incidents
-    accepts_nested_attributes_for :incidents, :allow_destroy => true
+  has_many :incidents
+  accepts_nested_attributes_for :incidents, :allow_destroy => true
+  validates :name, presence: true
+
+  validate :must_have_incident
+
+  def must_have_incident
+    if incidents.empty? or incidents.all? {|incident| incident.marked_for_destruction?}
+      errors.add(:base, 'Must have at least one incident')
+    end
+  end
 end

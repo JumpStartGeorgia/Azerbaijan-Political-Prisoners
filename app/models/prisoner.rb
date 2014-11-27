25b8c59a329674dc8f40301b4dfad28c8f7d81ce
nwshane <django09@gmail.com>
2014-11-27 12:39:46 +0400

    Simplified validate presence of incidents code.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,13 +1,5 @@
 class Prisoner < ActiveRecord::Base
   has_many :incidents
   accepts_nested_attributes_for :incidents, :allow_destroy => true
-  validates :name, presence: true
-
-  validate :must_have_incident
-
-  def must_have_incident
-    if incidents.empty? or incidents.all? {|incident| incident.marked_for_destruction?}
-      errors.add(:base, 'Must have at least one incident')
-    end
-  end
+  validates :name, :incidents, presence: true
 end

90193da260d18c676101f052717c1a059a2de48f
nwshane <django09@gmail.com>
2014-12-02 11:54:59 +0400

    Incident destroy will cause charges to be destroyed. Added article_ids to presence validation.

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -3,8 +3,8 @@ class Incident < ActiveRecord::Base
   belongs_to :prison
   belongs_to :type
   belongs_to :subtype
-  has_many :charges
+  has_many :charges, dependent: :destroy
   has_many :articles, through: :charges
 
-  validates :prisoner, :date_of_arrest, :prison_id, :type_id, presence: true
+  validates :prisoner_id, :date_of_arrest, :article_ids, :prison_id, :type_id, presence: true
 end

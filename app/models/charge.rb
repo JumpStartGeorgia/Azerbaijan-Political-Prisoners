e04122c820894362a7f4e4fa1989dca8ff2db4e2
nwshane <django09@gmail.com>
2014-12-02 11:49:27 +0400

    Charge model now validates presence of incident id and article id and also checks that the associated incident and article are valid.

--- app/models/charge.rb
+++ app/models/charge.rb
@@ -1,4 +1,7 @@
 class Charge < ActiveRecord::Base
   belongs_to :incident
   belongs_to :article
+
+  validates :incident_id, :article_id, presence: true
+  validates_associated :incident, :article
 end

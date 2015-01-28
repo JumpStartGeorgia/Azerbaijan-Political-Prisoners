dacc47541ff9325740e1901d36892ca64a0aecb7
nwshane <django09@gmail.com>
2015-01-28 11:42:22 +0400

    Wrote test showing that new prisoner page with one added incident displays two TinyMCE editors.

---
+++ spec/features/prisoner_features_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe "The prisoner new page", :type => :feature do
+  describe "with one added incident" do
+    it "loads two TinyMCE editors", :js => true do
+      visit '/prisoners/new'
+      click_link 'Add New Incident'
+      expect(page).to have_selector('.mce-tinymce', :count => 2)
+    end
+  end
+end

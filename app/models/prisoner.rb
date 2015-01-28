6ef907a8a42baf4a486d858862d9f63733ba0ff5
nwshane <django09@gmail.com>
2015-01-28 11:28:43 +0400

    Added missing.png for default medium sized portrait.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,6 +1,6 @@
 class Prisoner < ActiveRecord::Base
   has_many :incidents, inverse_of: :prisoner
-  has_attached_file :portrait, :styles => { :medium => "200x200>" }
+  has_attached_file :portrait, :styles => { :medium => "200x200>" }, :default_url => ":style/missing.png"
   validates_attachment :portrait, content_type: { content_type: /\Aimage\/.*\Z/ }
   accepts_nested_attributes_for :incidents, :allow_destroy => true
   validates :name, presence: true

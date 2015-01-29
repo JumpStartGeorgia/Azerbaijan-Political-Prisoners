d05012f8539ca74311627ec4d0708f6daf45f6db
nwshane <django09@gmail.com>
2015-01-29 09:54:14 +0400

    Replaced description text area on article form with tinymce editor.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_gon_variables
 
   # GET /articles
   # GET /articles.json
@@ -65,6 +66,10 @@ class ArticlesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_article
       @article = Article.find(params[:id])
+    end
+
+    def set_gon_variables
+      gon.tinymce_config = YAML.load_file("config/tinymce.yml")
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

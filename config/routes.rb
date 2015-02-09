ad2c42941b019ff348fc5a7cb4f533de4f964cbd
nwshane <django09@gmail.com>
2015-02-09 12:54:44 +0400

    Requires admin authentication to view resource views.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   devise_for :admins
 
-  resources :tags
+  authenticate :admin do
+    resources :tags
 
-  resources :prisons
+    resources :prisons
 
-  resources :criminal_codes
+    resources :criminal_codes
 
-  resources :articles
+    resources :articles
 
-  resources :prisoners
+    resources :prisoners
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

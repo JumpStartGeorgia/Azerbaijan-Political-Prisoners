99ad661276f6ff8fca9f261a34cf0d29c5eb2fb2
nwshane <django09@gmail.com>
2015-02-16 11:25:22 +0400

    Replaced user sign in check with cancan ability check to determine whether to display edit actions on article index.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def page_title(page_title)
     content_for(:page_title) { page_title.html_safe }
   end
+
+  def if_can_edit_or_destroy? resource
+    if can? :edit, resource or can? :destroy, resource
+      yield
+    end
+  end
 end

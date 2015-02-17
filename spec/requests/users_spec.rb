a24d9ae566e1ae661c841d25580bc4b4d368591e
nwshane <django09@gmail.com>
2015-02-17 10:59:59 +0400

    Added Warden stubbed authentication to request spec.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", :type => :request do
+  before(:context) do
+    @role = FactoryGirl.create(:role, name: 'user_manager')
+    @user = FactoryGirl.create(:user, email: "a@a.com", password: "12345678", role: @role)
+  end
+
   describe "GET /users" do
     it "works! (now write some real specs)" do
+      login_as(@user, scope: :user)
+
       get users_path
       expect(response).to have_http_status(200)
     end

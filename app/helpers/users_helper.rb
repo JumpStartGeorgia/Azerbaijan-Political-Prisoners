938bbd036ed52123425042b7ce58b4a0d668b734
nwshane <django09@gmail.com>
2015-02-23 10:41:09 +0400

    Only populating the roles select on the User form with roles that the current user is allowed to update/create.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+  def roles_editable_by_current_user
+    roles = []
+    Role.all.each do |role|
+      roles.append(role) if can? :create, User.new(email: 'assdfasdfasdf@asdfdsffsd.com', password: '1234231432143', role: role)
+    end
+
+    return roles
+  end
 end

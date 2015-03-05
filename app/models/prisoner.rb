9d13e256544c6751fcb40fc1eb5aedf5b8aa78e2
nwshane <django09@gmail.com>
2015-03-05 11:32:15 +0400

    Deleting imprisoned_count_timeline.json when a prisoner is updated.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,5 +1,6 @@
 class Prisoner < ActiveRecord::Base
   after_commit :update_currently_imprisoned
+  after_commit :delete_imprisoned_count_timeline_json
 
   has_many :incidents, inverse_of: :prisoner
   has_attached_file :portrait, :styles => { :medium => "200x200>" }, :default_url => ":style/missing.png"
@@ -121,6 +122,11 @@ class Prisoner < ActiveRecord::Base
         self.update_column(:currently_imprisoned, true)
       end
     end
+  end
+
+  def delete_imprisoned_count_timeline_json
+    path = Rails.public_path.join("imprisoned_count_timeline.json")
+    File.delete(path) if File.exists?(path)
   end
 
   def self.arrest_counts_by_day

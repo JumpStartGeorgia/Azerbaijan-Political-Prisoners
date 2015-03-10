df0f7e64cc81e404861b5108756265709a11eafd
nwshane <django09@gmail.com>
2015-03-10 12:11:26 +0400

    Filled bar chart showing number of prisoners by prison with real data.

--- app/models/prison.rb
+++ app/models/prison.rb
@@ -3,7 +3,28 @@ class Prison < ActiveRecord::Base
 
   def self.generate_prison_prisoner_count_chart_json
     File.open(Rails.public_path.join("chart_data/prison_prisoner_count_chart.json"), "w") do |f|
-      f.write({prison_names: ["Prison !!!", "prison dsafljkdsaf"], series_data: [12, 23]}.to_json)
+      f.write(prison_prisoner_count_chart.to_json)
     end
+  end
+
+  private
+
+  def self.prison_prisoner_count_chart
+    prison_names = []
+    prisoner_counts = []
+
+    prison_names_prisoner_counts.each do |prison_name_prisoner_count|
+      prison_names.append(prison_name_prisoner_count[:prison_name])
+      prisoner_counts.append(prison_name_prisoner_count[:prisoner_count])
+    end
+
+    Rails.logger.debug(prison_names)
+    Rails.logger.debug(prisoner_counts)
+
+    return {prison_names: prison_names, series_data: prisoner_counts}
+  end
+
+  def self.prison_names_prisoner_counts
+    return find_by_sql('select prisons.name as prison_name, count(*) as prisoner_count from incidents inner join prisons on incidents.prison_id = prisons.id group by prisons.name order by count(*) desc')
   end
 end

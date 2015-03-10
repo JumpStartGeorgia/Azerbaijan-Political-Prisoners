fb3f01861ff0d488406f98a982638438bfcfe2d9
nwshane <django09@gmail.com>
2015-03-10 13:37:36 +0400

    Decimals not allowed in bar charts.

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,52 +1,52 @@
 $(document).ready(function() {
     if ($('body').hasClass('root')) {
 
-        // The data for this graph is not complete, so the graph is erroneous until the data is updated. The prisoners in the database currently only include those who were imprisoned as of August 2014. Any prisoners who were arrested and released after the starting date of this timeline and before August 2014 would not be reflected in this data.
-//        $.ajax({
-//            url: 'chart_data/imprisoned_count_timeline.json',
-//            async: true,
-//            dataType: 'json',
-//            success: function (response) {
-//                $(function () {
-//                    $('#imprisoned-count-timeline').highcharts({
-//                        chart: {
-//                            zoomType: 'x',
-//                            resetZoomButton: {
-//                                position: {
-//                                    align: 'left',
-//                                    verticalAlign: 'top',
-//                                    x: 10,
-//                                    y: 10
-//                                }
-//                            }
-//                        },
-//                        title: {
-//                            text: 'Number of Political Prisoners in Azerbaijan from 2007 through Today'
-//                        },
-//                        subtitle: {
-//                            text: document.ontouchstart === undefined ?
-//                                'Click and drag in the plot area to zoom in' :
-//                                'Pinch the chart to zoom in'
-//                        },
-//                        xAxis: {
-//                            type: 'datetime',
-//                            minRange: 14 * 24 * 3600000 // fourteen days
-//                        },
-//                        yAxis: {
-//                            title: {
-//                                text: 'Number of Political Prisoners'
-//                            },
-//                            min: 0
-//                        },
-//                        series: [{
-//                            name: 'Number of Political Prisoners',
-//                            showInLegend: false,
-//                            data: response.data
-//                        }]
-//                    });
-//                });
-//            }
-//        });
+        $.ajax({
+            url: 'chart_data/imprisoned_count_timeline.json',
+            async: true,
+            dataType: 'json',
+            success: function (response) {
+                $(function () {
+                    $('#imprisoned-count-timeline').highcharts({
+                        chart: {
+                            zoomType: 'x',
+                            resetZoomButton: {
+                                position: {
+                                    align: 'left',
+                                    verticalAlign: 'top',
+                                    x: 10,
+                                    y: 10
+                                }
+                            }
+                        },
+                        title: {
+                            text: 'Number of Political Prisoners in Azerbaijan from 2007 through Today'
+                        },
+                        subtitle: {
+                            text: document.ontouchstart === undefined ?
+                                'Click and drag in the plot area to zoom in' :
+                                'Pinch the chart to zoom in'
+                        },
+                        xAxis: {
+                            type: 'datetime',
+                            minRange: 14 * 24 * 3600000 // fourteen days
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Number of Political Prisoners'
+                            },
+                            min: 0,
+                            allowDecimals: false
+                        },
+                        series: [{
+                            name: 'Number of Political Prisoners',
+                            showInLegend: false,
+                            data: response.data
+                        }]
+                    });
+                });
+            }
+        });
 
         $.ajax({
             url: 'chart_data/prison_prisoner_count_chart.json',
@@ -63,10 +63,11 @@ $(document).ready(function() {
                     yAxis: {
                         title: {
                             text: 'Number of Prisoners'
-                        }
+                        },
+                        allowDecimals: false
                     },
                     xAxis: {
-                        categories: response.prison_names,
+                        categories: response.prison_names
                     },
                     series: [{
                         name: 'Number of Prisoners',
@@ -92,7 +93,8 @@ $(document).ready(function() {
                     yAxis: {
                         title: {
                             text: 'Number of Sentences'
-                        }
+                        },
+                        allowDecimals: false
                     },
                     xAxis: {
                         categories: response.article_numbers,

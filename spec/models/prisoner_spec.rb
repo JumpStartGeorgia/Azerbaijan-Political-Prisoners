52605aa80e5c1f406d32e786a83360b6d732f21a
nwshane <django09@gmail.com>
2015-03-11 14:19:59 +0400

    Added spec for prisoner imprisoned ids at date.

--- spec/models/prisoner_spec.rb
+++ spec/models/prisoner_spec.rb
@@ -41,4 +41,43 @@ RSpec.describe Prisoner, :type => :model do
       expect { p1.save! }.not_to raise_error
     end
   end
+
+  it "returns the right number of imprisoned for a certain date" do
+    # Two prisoners, each with one incident and no date of release
+    p1 = FactoryGirl.create(:prisoner)
+    p1.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2012, 1, 1))
+    p1.run_callbacks(:commit)
+
+    p2 = FactoryGirl.create(:prisoner)
+    p2.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2014, 1, 1))
+    p2.run_callbacks(:commit)
+
+    # Prisoner with two incidents, first has date of release and second does not
+    p3 = FactoryGirl.create(:prisoner)
+    p3.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2011, 3, 4), date_of_release: Date.new(2012, 2, 11))
+    p3.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2014, 1, 1))
+    p3.run_callbacks(:commit)
+
+    # Prisoner with two incidents, both of which have date of release
+    p4 = FactoryGirl.create(:prisoner)
+    p4.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2009, 8, 5), date_of_release: Date.new(2012, 12, 3))
+    p4.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2014, 12, 5), date_of_release: Date.new(2014, 12, 8))
+    p4.run_callbacks(:commit)
+
+    # Prisoner with three incidents, all three have date of release
+    p5 = FactoryGirl.create(:prisoner)
+    p5.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2005, 3, 5), date_of_release: Date.new(2007, 12, 8))
+    p5.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2008, 12, 5), date_of_release: Date.new(2009, 12, 8))
+    p5.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2010, 12, 5), date_of_release: Date.new(2013, 12, 8))
+    p5.run_callbacks(:commit)
+
+    # Prisoner with no incidents
+    p6 = FactoryGirl.create(:prisoner)
+    p6.run_callbacks(:commit)
+
+    expect(Prisoner.imprisoned_count(Date.new(2013, 1, 1))).to eq(2)
+
+    imprisoned = Prisoner.imprisoned_ids(Date.new(2013, 1, 1))
+    expect(imprisoned).to eq([1, 5])
+  end
 end

926870fd45bf412879098892d89f3f3c1c5c4c31
nwshane <django09@gmail.com>
2015-03-11 10:54:05 +0400

    Removed one year ago prisoner count from home page.

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,7 +1,6 @@
 class RootController < ApplicationController
   def index
     @currently_imprisoned_count = Prisoner.currently_imprisoned_count
-    @one_year_ago_imprisoned_count = Prisoner.imprisoned_count(1.year.ago)
 
     if !File.exists?(Rails.public_path.join('chart_data/imprisoned_count_timeline.json'))
       Prisoner.generate_imprisoned_count_timeline_json

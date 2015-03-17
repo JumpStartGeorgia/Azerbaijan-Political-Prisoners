e100df4979184b3e2de269f9653aacc1c6b3eb6c
nwshane <django09@gmail.com>
2015-03-17 10:58:54 +0400

    Added an editing prisoner step to the update json data feature spec.

--- spec/features/json_data_features_spec.rb
+++ spec/features/json_data_features_spec.rb
@@ -6,56 +6,27 @@ RSpec.describe 'JSON data', type: :feature do
     @user = FactoryGirl.create(:user, role: @role)
   end
 
-  it "for charts update when a new prisoner is created", js: true do
+  it "for charts update when a new prisoner is created and when a prisoner is updated", js: true do
     login_as(@user, scope: :user)
 
-    prison1 = FactoryGirl.create(:prison, name: 'prison#1')
-    prison2 = FactoryGirl.create(:prison, name: 'prison#2')
+    FactoryGirl.create(:prison, name: 'prison#1')
+    FactoryGirl.create(:prison, name: 'prison#2')
 
-    article1 = FactoryGirl.create(:article, number: 'article#1')
-    article2 = FactoryGirl.create(:article, number: 'article#2')
-
-    p1 = FactoryGirl.create(:prisoner)
-    i1 = FactoryGirl.create(:incident, date_of_arrest: Date.new(2012, 1, 1), prison: prison1)
-    i1.articles << article1
-    p1.incidents << i1
-    p1.run_callbacks(:commit)
-
-    p2 = FactoryGirl.create(:prisoner)
-    i2 = FactoryGirl.create(:incident, date_of_arrest: Date.new(2012, 1, 1), prison: prison2)
-    i2.articles << article2
-    p2.incidents << i2
-    p2.run_callbacks(:commit)
-
-    p3 = FactoryGirl.create(:prisoner)
-    p3.incidents << FactoryGirl.create(:incident, date_of_arrest: Date.new(2014, 1, 1), prison: prison1)
-    p3.run_callbacks(:commit)
+    FactoryGirl.create(:article, number: 'article#1')
+    FactoryGirl.create(:article, number: 'article#2')
 
     timeline_json_path = "/chart_data/imprisoned_count_timeline"
     prison_prisoner_counts_json_path = "/chart_data/prison_prisoner_counts"
     article_incident_counts_json_path = "/chart_data/article_incident_counts"
 
-    # Visit root and save json data in variables
-    visit root_path
-
     visit timeline_json_path
-    old_timeline_json = page.body
+    timeline_json1 = page.body
 
     visit prison_prisoner_counts_json_path
-    old_prison_prisoner_counts_json = page.body
+    prison_prisoner_counts_json1 = page.body
 
     visit article_incident_counts_json_path
-    old_article_incident_counts_json = page.body
-
-    ##Add date of release to p3
-    #visit edit_prisoner_path(p3)
-    #within('.nested-fields') do
-    #  fill_in 'Date of arrest', with: '2015-01-01'
-    #  #fill_in 'Date of release', with: '2015-02-10'
-    #end
-    #
-    #click_button 'Update Prisoner'
-    #expect(page).to have_content("Prisoner was successfully updated.")
+    article_incident_counts_json1 = page.body
 
     visit new_prisoner_path
 
@@ -67,7 +38,7 @@ RSpec.describe 'JSON data', type: :feature do
 
     within('.nested-fields') do
       fill_in 'Date of arrest', with: '2015-01-01'
-      select('prison#2', from: 'Prison')
+      select('prison#1', from: 'Prison')
     end
 
     select2_select_multiple(['article#1'], find(:xpath, "//*[contains(@id, 's2id_prisoner_incidents_attributes')][contains(@id, 'article_ids')]//input"))
@@ -75,20 +46,43 @@ RSpec.describe 'JSON data', type: :feature do
     click_button 'Create Prisoner'
     expect(page).to have_content('Prisoner was successfully created.')
 
-    # Visit root and compare old json data to new json data
+    visit timeline_json_path
+    timeline_json2 = page.body
+    expect(timeline_json1).not_to eq(timeline_json2)
+
+    visit prison_prisoner_counts_json_path
+    prison_prisoner_counts_json2 = page.body
+    expect(prison_prisoner_counts_json1).not_to eq(prison_prisoner_counts_json2)
+
+    visit article_incident_counts_json_path
+    article_incident_counts_json2 = page.body
+    expect(article_incident_counts_json1).not_to eq(article_incident_counts_json2)
+
+    visit edit_prisoner_path(Prisoner.find_by_name('Bob Jones').id)
+
+    within('.nested-fields') do
+      fill_in 'Date of release', with: '2015-02-10'
+      select('prison#2', from: 'Prison')
+    end
+
+    select2_select_multiple(['article#2'], find(:xpath, "//*[contains(@id, 's2id_prisoner_incidents_attributes')][contains(@id, 'article_ids')]//input"))
+
+    click_button 'Update Prisoner'
+    expect(page).to have_content("Prisoner was successfully updated.")
+
     visit root_path
 
     visit timeline_json_path
-    new_timeline_json = page.body
-    expect(old_timeline_json).not_to eq(new_timeline_json)
+    timeline_json3 = page.body
+    expect(timeline_json2).not_to eq(timeline_json3)
 
     visit prison_prisoner_counts_json_path
-    new_prison_prisoner_counts_json = page.body
-    expect(old_prison_prisoner_counts_json).not_to eq(new_prison_prisoner_counts_json)
+    prison_prisoner_counts_json3 = page.body
+    expect(prison_prisoner_counts_json2).not_to eq(prison_prisoner_counts_json3)
 
     visit article_incident_counts_json_path
-    new_article_incident_counts_json = page.body
-    expect(old_article_incident_counts_json).not_to eq(new_article_incident_counts_json)
+    article_incident_counts_json3 = page.body
+    expect(article_incident_counts_json2).not_to eq(article_incident_counts_json3)
   end
 end
 

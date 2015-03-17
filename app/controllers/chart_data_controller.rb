ad3dc94409fcc4dcde041419d0f27f4712b3099b
nwshane <django09@gmail.com>
2015-03-17 10:10:04 +0400

    Chart data controller actions now check whether json data exists (and creates it if it doesn't) instead of root controller. Closes #219

--- app/controllers/chart_data_controller.rb
+++ app/controllers/chart_data_controller.rb
@@ -1,13 +1,25 @@
 class ChartDataController < ApplicationController
   def imprisoned_count_timeline
+    if !File.exists?(Rails.public_path.join('chart_data/imprisoned_count_timeline.json'))
+      Prisoner.generate_imprisoned_count_timeline_json
+    end
+
     respond_with_public_json('chart_data/imprisoned_count_timeline.json')
   end
 
   def prison_prisoner_counts
+    if !File.exists?(Rails.public_path.join('chart_data/prison_prisoner_count_chart.json'))
+      Prison.generate_prison_prisoner_count_chart_json
+    end
+
     respond_with_public_json('chart_data/prison_prisoner_count_chart.json')
   end
 
   def article_incident_counts
+    if !File.exists?(Rails.public_path.join('chart_data/article_incident_counts_chart.json'))
+      Article.generate_highest_incident_counts_chart_json
+    end
+
     respond_with_public_json('chart_data/article_incident_counts_chart.json')
   end
 
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,17 +1,5 @@
 class RootController < ApplicationController
   def index
     @currently_imprisoned_count = Prisoner.currently_imprisoned_count
-
-    if !File.exists?(Rails.public_path.join('chart_data/imprisoned_count_timeline.json'))
-      Prisoner.generate_imprisoned_count_timeline_json
-    end
-
-    if !File.exists?(Rails.public_path.join('chart_data/prison_prisoner_count_chart.json'))
-      Prison.generate_prison_prisoner_count_chart_json
-    end
-
-    if !File.exists?(Rails.public_path.join('chart_data/article_incident_counts_chart.json'))
-      Article.generate_highest_incident_counts_chart_json
-    end
   end
 end

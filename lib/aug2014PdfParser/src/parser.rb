4fd2ecb8645c848c26f6e2d226c9585b6ec87b9d
nwshane <django09@gmail.com>
2015-03-23 13:57:05 +0400

    Added Aug2014PdfParser module and running it from rake aug2014_data:migrate task.

--- lib/aug2014PdfParser/src/parser.rb
+++ lib/aug2014PdfParser/src/parser.rb
@@ -3,23 +3,27 @@ require 'csv'
 require_relative 'List.rb'
 
 #Create list object containing cleaned and structured HTML
-input_path = File.dirname(__FILE__) + '/../input/list.html'
-list = List.new(input_path)
+module Aug2014ListParser
+  def self.parse
+    input_path = File.dirname(__FILE__) + '/../input/list.html'
+    list = List.new(input_path)
 
 #Find the prisoner type sections in the list
-list.findPrisonerTypes
+    list.findPrisonerTypes
 
 #Find the prisoners in each of those sections
-list.getPrisonerTypes.each do |prisonerType|
-    prisonerType.findPrisoners
-end
+    list.getPrisonerTypes.each do |prisonerType|
+      prisonerType.findPrisoners
+    end
 
 #Output values to CSV
-prisonersOutputPath = File.dirname(__FILE__) + '/../output/prisoners.csv'
-subtypesOutputPath = File.dirname(__FILE__) + '/../output/subtypes.csv'
-placesOfDetentionOutputPath = File.dirname(__FILE__) + '/../output/placesOfDetention.csv'
-articlesOutputPath = File.dirname(__FILE__) + '/../output/articles.csv'
-list.outputPrisoners(prisonersOutputPath)
-list.outputSubtypes(subtypesOutputPath)
-list.outputPlacesOfDetention(placesOfDetentionOutputPath)
-list.outputArticles(articlesOutputPath)
+    prisonersOutputPath = File.dirname(__FILE__) + '/../output/prisoners.csv'
+    subtypesOutputPath = File.dirname(__FILE__) + '/../output/subtypes.csv'
+    placesOfDetentionOutputPath = File.dirname(__FILE__) + '/../output/placesOfDetention.csv'
+    articlesOutputPath = File.dirname(__FILE__) + '/../output/articles.csv'
+    list.outputPrisoners(prisonersOutputPath)
+    list.outputSubtypes(subtypesOutputPath)
+    list.outputPlacesOfDetention(placesOfDetentionOutputPath)
+    list.outputArticles(articlesOutputPath)
+  end
+end
--- lib/tasks/migrate_aug2014_data.rake
+++ lib/tasks/migrate_aug2014_data.rake
@@ -3,7 +3,8 @@ namespace :aug2014_data do
   desc 'Transfer the data from the August 2014 Azeri Prisoner List (stored at lib/aug2014PdfParser/input/list.html) into Rails database'
   task :migrate => :environment do
     puts 'Parsing Azeri prisoner list data into lib/aug2014PdfParser/output'
-
+    require 'aug2014PdfParser/src/parser'
+    Aug2014ListParser.parse
     puts 'Finished parsing Azeri prisoner list data into lib/aug2014PdfParser/output'
 
     puts 'Migrating Azeri prisoner list data from lib/aug2014PdfParser/output into database; also transferring portraits from lib/aug2014PdfParser/portraits into public/system/portraits'

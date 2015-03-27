78a25c00bb093cf804a7de57b914316b529a9362
nwshane <django09@gmail.com>
2015-03-27 17:34:32 +0400

    Added line activate_control_app to puma config.

--- config/puma.rb
+++ config/puma.rb
@@ -6,7 +6,10 @@ environment 'staging'
 
 pidfile "/home/prisoners-staging/Azeri-Prisoners-Staging/shared/tmp/pids/puma.pid"
 state_path "/home/prisoners-staging/Azeri-Prisoners-Staging/shared/tmp/sockets/puma.state"
+activate_control_app
+
 stdout_redirect '/home/prisoners-staging/Azeri-Prisoners-Staging/shared/log/puma.error.log', '/home/prisoners-staging/Azeri-Prisoners-Staging/shared/log/puma.access.log', true
+
 
 
 threads 4,16

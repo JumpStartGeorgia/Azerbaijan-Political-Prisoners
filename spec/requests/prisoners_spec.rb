1ef1c1fdc84d6f92454d7f3e55626f9fe559f9f4
nwshane <django09@gmail.com>
2015-05-14 12:44:45 +0400

    Added request specs for chart data.

--- spec/requests/prisoners_spec.rb
+++ spec/requests/prisoners_spec.rb
@@ -2,8 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "Prisoners", :type => :request do
   describe "GET /prisoners" do
-    it "works! (now write some real specs)" do
+    it "works" do
       get prisoners_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET /prisoners/imprisoned_count_timeline" do
+    it "works" do
+      get imprisoned_count_timeline_prisoners_path
       expect(response).to have_http_status(200)
     end
   end

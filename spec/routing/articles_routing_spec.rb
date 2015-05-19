f09f1d19d586226c6c9779fb1f7e3956b8424f5d
nwshane <django09@gmail.com>
2015-05-19 11:36:27 +0400

    Requiring pry in rspec.

--- spec/routing/articles_routing_spec.rb
+++ spec/routing/articles_routing_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe ArticlesController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
+      binding.pry
       expect(get: '/articles').to route_to('articles#index')
     end
 

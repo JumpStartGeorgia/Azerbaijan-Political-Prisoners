6638c04ba716cce36847a4e0829398d1df2d22ad
nwshane <django09@gmail.com>
2015-05-28 12:59:26 +0400

    Prisoner destroy now destroys dependent incidents.

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,5 +1,5 @@
 class Prisoner < ActiveRecord::Base
-  has_many :incidents, inverse_of: :prisoner
+  has_many :incidents, inverse_of: :prisoner, dependent: :destroy
   has_attached_file :portrait,
                     styles: { medium: '200x200>' },
                     default_url: '/images/:class/:attachment/:style/missing.png',

6e375c8ba2936630910b63355068ea547c16e50d
nwshane <django09@gmail.com>
2015-06-05 10:55:17 +0400

    Migration to add paragraph tags to tag descriptions that don't already have them.

---
+++ db/migrate/20150605063220_add_p_tags_to_tag_descriptions.rb
@@ -0,0 +1,21 @@
+class AddPTagsToTagDescriptions < ActiveRecord::Migration
+  def up
+    Tag.transaction do
+      Tag.all.each do |tag|
+        puts "---------------------------------------------"
+        puts "id = #{tag.id}"
+        puts "desc = #{tag.description}"
+        if tag.description.present? && !tag.description.start_with?('<p', '<P')
+          puts ">>>>> has desc that does not start with p, applying simple format"
+          tag.description = ActionController::Base.helpers.simple_format(tag.description)
+          puts "new desc = #{tag.description}"
+        end
+
+        tag.save
+      end
+    end
+  end
+
+  def down
+  end
+end

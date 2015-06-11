d664b8714350002bb56c975cdb7ecc7418405ecb
nwshane <django09@gmail.com>
2015-06-11 10:29:57 +0400

    Changed staging branch to master.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,6 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Azeri-Prisoners-Staging'
 set :repository, 'git@github.com:JumpStartGeorgia/Azerbaijan-Political-Prisoners.git'
-set :repo_branch, 'puma'
+set :repo_branch, 'master'
 set :web_url, 'dev-prisoners.jumpstart.ge'
 set :use_ssl, true

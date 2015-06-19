0d67007e049f9c9e757aff4790e60c435b2dbf04
nwshane <django09@gmail.com>
2015-06-19 10:36:05 +0400

    Turned on bullet.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   Paperclip.options[:command_path] = '/usr/local/bin/'
-  bullet = false
+  bullet = true
   config.after_initialize do
     Bullet.enable = bullet
     Bullet.alert = bullet

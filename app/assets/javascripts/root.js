489ed274e3b296346302800f73a3d45192abe4c9
nwshane <django09@gmail.com>
2015-06-19 11:42:21 +0400

    Refactored home page js.

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
-    if ($('body').hasClass('root')) {
-      $(window).resize(function() {
-        imprisoned_count_timeline();
-        prison_prisoner_counts_chart();
-        article_incident_counts_chart();
-      })
-
-      imprisoned_count_timeline();
-      prison_prisoner_counts_chart();
-      article_incident_counts_chart();
-    }
+  // Run only on home page
+  if ($('body').hasClass('root index')) {
+    homePage();
+  }
 });
+
+function homePage() {
+  $(window).resize(function() {
+    loadCharts();
+  })
+
+  loadCharts();
+}
+
+function loadCharts() {
+  imprisoned_count_timeline();
+  prison_prisoner_counts_chart();
+  article_incident_counts_chart();
+}

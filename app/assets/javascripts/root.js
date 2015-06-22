c789195d8244a5fece8554ea94f0804891b6a2af
nwshane <django09@gmail.com>
2015-06-22 10:47:44 +0400

    ESLint fixes in root.js.

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -1,20 +1,20 @@
-$(document).ready(function() {
-  // Run only on home page
-  if ($('body').hasClass('root index')) {
-    homePage();
-  }
-});
-
-function homePage() {
-  $(window).resize(function() {
-    loadCharts();
-  })
-
-  loadCharts();
-}
-
 function loadCharts() {
   imprisonedCountTimeline();
   prison_prisoner_counts_chart();
   article_incident_counts_chart();
 }
+
+function homePage() {
+  $(window).resize(function() {
+    loadCharts();
+  });
+
+  loadCharts();
+}
+
+$(document).ready(function() {
+  // Run only on home page
+  if ($("body").hasClass("root index")) {
+    homePage();
+  }
+});

33c798871c9ff0c312da175809ef5f8ec99bed17
nwshane <django09@gmail.com>
2015-06-23 13:06:27 +0400

    Fading in and fading out loading indicator.

--- app/assets/javascripts/loading_indicator.js
+++ app/assets/javascripts/loading_indicator.js
@@ -1,9 +1,9 @@
 // Shows and hides the loading indicator when users switch pages
 
 $(document).on("page:fetch", function() {
-  $(".loading-indicator").show();
+  $(".loading-indicator").fadeIn();
 });
 
 $(document).on("page:change", function() {
-  $(".loading-indicator").hide();
+  $(".loading-indicator").fadeOut();
 });

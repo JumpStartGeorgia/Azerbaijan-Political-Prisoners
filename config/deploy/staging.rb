6de0478174042b0bc7bc35691cbed34473a3eaf6
nwshane <django09@gmail.com>
2015-06-24 10:09:52 +0400

    Changed staging branch.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,6 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Azeri-Prisoners-Staging'
 set :repository, 'git@github.com:JumpStartGeorgia/Azerbaijan-Political-Prisoners.git'
-set :repo_branch, 'master'
+set :repo_branch, '322'
 set :web_url, 'dev-prisoners.jumpstart.ge'
 set :use_ssl, true

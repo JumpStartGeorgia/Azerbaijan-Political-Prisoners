5415bd6f636edef5d4ba757a4ac7f9d3c1624167
nwshane <django09@gmail.com>
2015-06-30 10:57:11 +0400

    Resetting article slugs after criminal code is saved.

--- app/models/criminal_code.rb
+++ app/models/criminal_code.rb
@@ -6,6 +6,15 @@ class CriminalCode < ActiveRecord::Base
   # strip extra spaces before saving
   auto_strip_attributes :name
 
+  # Recreates the article slugs in case criminal code name changed
+  after_save :save_articles
+  def save_articles
+    articles.each do |article|
+      article.slug = nil
+      article.save!
+    end
+  end
+
   def name_with_model_name
     name + ' Criminal Code'
   end

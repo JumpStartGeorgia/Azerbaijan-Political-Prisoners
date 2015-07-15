0f510338359404688d6df05bb48c609cb80f1255
nwshane <django09@gmail.com>
2015-07-15 11:59:39 +0400

    Added real page path.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,10 @@
 module PagesHelper
+  def real_page_path(page)
+    begin
+      eval("#{page.name}_path")
+    rescue NameError
+      page.errors.add(:name, 'does not match any real page')
+      return '#'
+    end
+  end
 end

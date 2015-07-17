29e9779bc6d8cab3460fd916e760c667f2cbb9f2
nwshane <django09@gmail.com>
2015-07-17 11:27:53 +0400

    Added presence and uniqueness specs to page section model spec.

--- spec/models/page_section_spec.rb
+++ spec/models/page_section_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe PageSection, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:page_section1) { FactoryGirl.build(:page_section,
+                                           name: 'app_intro') }
+  let (:page_section2) { FactoryGirl.build(:page_section,
+                                           name: 'project_description') }
+
+  describe 'cannot be saved' do
+    it 'without name' do
+      page_section1.name = ''
+      expect { page_section1.save! }.to raise_error
+    end
+
+    it 'with same name' do
+      page_section1.save!
+      page_section2.name = page_section1.name
+      expect { page_section2.save! }.to raise_error
+    end
+  end
 end

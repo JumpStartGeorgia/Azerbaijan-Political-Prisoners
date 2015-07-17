77832d6b2934be30c31ad1021ba0efe0352459e1
nwshane <django09@gmail.com>
2015-07-17 11:32:00 +0400

    Fixed page section request spec.

--- spec/requests/page_sections_spec.rb
+++ spec/requests/page_sections_spec.rb
@@ -1,9 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe "Pages", :type => :request do
-  describe "GET /pages" do
-    it "works! (now write some real specs)" do
-      get pages_path
+RSpec.describe "PageSections", type: :request do
+  before(:context) do
+    @role = FactoryGirl.create(:role, name: 'site_admin')
+    @site_admin = FactoryGirl.create(:user, role: @role)
+  end
+
+  describe "GET /page_sections" do
+    it "works" do
+      login_as(@site_admin, scope: :user)
+
+      get page_sections_path
       expect(response).to have_http_status(200)
     end
   end

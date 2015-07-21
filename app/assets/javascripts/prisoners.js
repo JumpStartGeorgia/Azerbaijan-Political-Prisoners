04c3f7dc9b34f6b1cfaddd471428d81989f50a25
nwshane <django09@gmail.com>
2015-07-21 10:52:34 +0400

    Changed javascript 4 space tabs to 2 space tabs.

--- app/assets/javascripts/prisoners.js
+++ app/assets/javascripts/prisoners.js
@@ -1,23 +1,23 @@
 // Add jQuery Select2 extra functionality to Charges multiple select
 function addSelect2() {
-    $("select.charges_select").select2({
-        placeholder: "Select Charges",
-        width: "400px"
-    });
+  $("select.charges_select").select2({
+    placeholder: "Select Charges",
+    width: "400px"
+  });
 
-    $("select.tags_select").select2({
-        placeholder: "Select Tags",
-        width: "400px"
-    });
+  $("select.tags_select").select2({
+    placeholder: "Select Tags",
+    width: "400px"
+  });
 }
 
 function addDatePickers() {
-    $(".date_of_arrest_select, .date_of_release_select, .date_of_birth_select").datepicker({
-        dateFormat: "yy-mm-dd",
-        changeMonth: true,
-        changeYear: true,
-        yearRange: "-100:+0"
-    });
+  $(".date_of_arrest_select, .date_of_release_select, .date_of_birth_select").datepicker({
+    dateFormat: "yy-mm-dd",
+    changeMonth: true,
+    changeYear: true,
+    yearRange: "-100:+0"
+  });
 }
 
 $(document).ready(function() {
@@ -42,69 +42,69 @@ $(document).ready(function() {
 });
 
 $(document).on("page:receive", function() {
-    tinymce.remove();
+  tinymce.remove();
 });
 
 function imprisonedCountTimeline() {
-    $.ajax({
-        url: gon.imprisoned_count_timeline_prisoners_path,
-        async: true,
-        dataType: "json",
-        success: function (response) {
-            $(function () {
-                $("#imprisoned-count-timeline").highcharts({
-                    chart: {
-                        zoomType: "x",
-                        pinchType: "none",
-                        resetZoomButton: {
-                            position: {
-                                align: "left",
-                                verticalAlign: "top",
-                                x: 10,
-                                y: 10
-                            }
-                        }
-                    },
-                    title: {
-                        text: "How has the number of political prisoners changed over time?",
-                        useHTML: true
-                    },
-                    subtitle: {
-                        text: "<a href=\"" + gon.prisoners_path + "\">Click here to explore prisoners</a>",
-                        useHTML: true
-                    },
-                    xAxis: {
-                        type: "datetime",
-                        minRange: 14 * 24 * 3600000 // fourteen days
-                    },
-                    yAxis: {
-                        title: {
-                            text: "Number of Political Prisoners"
-                        },
-                        min: 0,
-                        allowDecimals: false
-                    },
-                    tooltip: {
-                        formatter: function() {
-                            var prisonerCount = this.point.y;
-                            var date = Highcharts.dateFormat("%A, %b %e, %Y", new Date(this.point.x));
-                            if ( prisonerCount === "1" ) {
-                                return "There was <strong>" + prisonerCount + "</strong> political prisoner in Azerbaijan on <strong>" + date + "</strong>";
-                            }
-                            else {
-                                return "There were <strong>" + prisonerCount + "</strong> political prisoners in Azerbaijan on <strong>" + date + "</strong>";
-                            }
-                        },
-                        useHTML: true,
-                        style: { padding: "1px" }
-                    },
-                    series: [{
-                        name: "Number of Political Prisoners",
-                        showInLegend: false,
-                        data: response
-                    }]
-                });
-            });
-        }
-    });
+  $.ajax({
+    url: gon.imprisoned_count_timeline_prisoners_path,
+    async: true,
+    dataType: "json",
+    success: function (response) {
+      $(function () {
+        $("#imprisoned-count-timeline").highcharts({
+          chart: {
+            zoomType: "x",
+            pinchType: "none",
+            resetZoomButton: {
+              position: {
+                align: "left",
+                verticalAlign: "top",
+                x: 10,
+                y: 10
+              }
+            }
+          },
+          title: {
+            text: "How has the number of political prisoners changed over time?",
+            useHTML: true
+          },
+          subtitle: {
+            text: "<a href=\"" + gon.prisoners_path + "\">Click here to explore prisoners</a>",
+            useHTML: true
+          },
+          xAxis: {
+            type: "datetime",
+            minRange: 14 * 24 * 3600000 // fourteen days
+          },
+          yAxis: {
+            title: {
+              text: "Number of Political Prisoners"
+            },
+            min: 0,
+            allowDecimals: false
+          },
+          tooltip: {
+            formatter: function() {
+              var prisonerCount = this.point.y;
+              var date = Highcharts.dateFormat("%A, %b %e, %Y", new Date(this.point.x));
+              if ( prisonerCount === "1" ) {
+                  return "There was <strong>" + prisonerCount + "</strong> political prisoner in Azerbaijan on <strong>" + date + "</strong>";
+              }
+              else {
+                  return "There were <strong>" + prisonerCount + "</strong> political prisoners in Azerbaijan on <strong>" + date + "</strong>";
+              }
+            },
+            useHTML: true,
+            style: { padding: "1px" }
+          },
+          series: [{
+            name: "Number of Political Prisoners",
+            showInLegend: false,
+            data: response
+          }]
+        });
+      });
+    }
+  });
 }
--- app/assets/javascripts/prisons.js
+++ app/assets/javascripts/prisons.js
@@ -1,99 +1,98 @@
 $(document).ready(function() {
-    if ($('body').hasClass('prisons')) {
-        loadTinymce();
-    }
+  if ($('body').hasClass('prisons')) {
+    loadTinymce();
+  }
 });
 
 function prison_prisoner_counts_chart() {
-    $.ajax({
-        url: gon.prison_prisoner_counts_prisons_path,
-        async: true,
-        dataType: 'json',
-        success: function (response) {
-            data = response.data;
-            text = response.text;
+  $.ajax({
+    url: gon.prison_prisoner_counts_prisons_path,
+    async: true,
+    dataType: 'json',
+    success: function (response) {
+      data = response.data;
+      text = response.text;
 
-            Highcharts.setOptions({
-              lang: {
-                contextButtonTitle: text.highcharts.context_title
-              }
-            });
+      Highcharts.setOptions({
+        lang: {
+          contextButtonTitle: text.highcharts.context_title
+        }
+      });
 
-            $('#prison-prisoner-counts').highcharts({
-                chart: {
-                    type: 'bar'
+      $('#prison-prisoner-counts').highcharts({
+        chart: {
+          type: 'bar'
+        },
+        title: {
+          text: text.title,
+          useHTML: true
+        },
+        subtitle: {
+          text: '<a href="' + text.prisons_path + '">' + text.explore_prisons + '</a>',
+          useHTML: true
+        },
+        yAxis: {
+          title: {
+            text: text.number_prisoners
+          },
+          allowDecimals: false
+        },
+        xAxis: {
+          categories: data,
+          labels: {
+            formatter: function() {
+              return '<a href="' + this.value.link + '">' + this.value.name + '</a>';
+            },
+            useHTML: true,
+            step: 1,
+            style: { 'textAlign': 'right' }
+          }
+        },
+        tooltip: {
+          formatter: function() {
+            return this.point.summary;
+          },
+          useHTML: true,
+          style: { padding: '1px' }
+        },
+        series: [{
+          name: text.number_prisoners,
+          showInLegend: false,
+          data: data
+        }],
+        exporting: {
+          buttons: {
+            contextButton: {
+              menuItems: [
+                {
+                  text: text.highcharts.png,
+                  onclick: function () {
+                    this.exportChart({type: 'image/png'});
+                  }
                 },
-                title: {
-                    text: text.title,
-                    useHTML: true
+                {
+                  text: text.highcharts.jpg,
+                  onclick: function () {
+                    this.exportChart({type: 'image/jpeg'});
+                  }
                 },
-                subtitle: {
-                    text: '<a href="' + text.prisons_path + '">' + text.explore_prisons + '</a>',
-                    useHTML: true
+                {
+                  text: text.highcharts.pdf,
+                  onclick: function () {
+                    this.exportChart({type: 'application/pdf'});
+                  }
                 },
-                yAxis: {
-                    title: {
-                        text: text.number_prisoners
-                    },
-                    allowDecimals: false
-                },
-                xAxis: {
-                    categories: data,
-                    labels: {
-                        formatter: function() {
-                            return '<a href="' + this.value.link + '">' + this.value.name + '</a>';
-                        },
-                        useHTML: true,
-                        step: 1,
-                        style: { 'textAlign': 'right' }
-                    }
-
-                },
-                tooltip: {
-                    formatter: function() {
-                        return this.point.summary;
-                    },
-                    useHTML: true,
-                    style: { padding: '1px' }
-                },
-                series: [{
-                    name: text.number_prisoners,
-                    showInLegend: false,
-                    data: data
-                }],
-                exporting: {
-                  buttons: {
-                    contextButton: {
-                      menuItems: [
-                        {
-                          text: text.highcharts.png,
-                          onclick: function () {
-                              this.exportChart({type: 'image/png'});
-                          }
-                        },
-                        {
-                          text: text.highcharts.jpg,
-                          onclick: function () {
-                              this.exportChart({type: 'image/jpeg'});
-                          }
-                        },
-                        {
-                          text: text.highcharts.pdf,
-                          onclick: function () {
-                              this.exportChart({type: 'application/pdf'});
-                          }
-                        },
-                        {
-                          text: text.highcharts.svg,
-                          onclick: function () {
-                              this.exportChart({type: 'image/svg+xml'});
-                          }
-                        }
-                      ]
-                    }
+                {
+                  text: text.highcharts.svg,
+                  onclick: function () {
+                    this.exportChart({type: 'image/svg+xml'});
                   }
                 }
-            });
+              ]
+            }
+          }
         }
-    });
-};
+      });
+    }
+  });
+}

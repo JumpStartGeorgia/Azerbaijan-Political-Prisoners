2582a0e40fe7590e808e0c7fecb04413fb1ee4d1
nwshane <django09@gmail.com>
2015-07-28 11:41:07 +0400

    Fixed criminal code controller specs.

--- spec/controllers/criminal_codes_controller_spec.rb
+++ spec/controllers/criminal_codes_controller_spec.rb
@@ -29,9 +29,9 @@ RSpec.describe CriminalCodesController, type: :controller do
   # This should return the minimal set of attributes required to create a valid
   # CriminalCode. As you add validations to CriminalCode, be sure to
   # adjust the attributes here as well.
-  let(:valid_attributes) { FactoryGirl.attributes_for(:criminal_code, name: 'MyName') }
+  let(:valid_attributes) { FactoryGirl.attributes_for(:criminal_code) }
 
-  let(:invalid_attributes) { FactoryGirl.attributes_for(:criminal_code, name: '') }
+  let(:invalid_attributes) { FactoryGirl.attributes_for(:criminal_code, name_en: '') }
 
   # This should return the minimal set of values that should be in the session
   # in order to pass any filters (e.g. authentication) defined in

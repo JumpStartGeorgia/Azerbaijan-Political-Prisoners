ddf99ca986ee289c266f8ea55af403b9d45c5697
nwshane <django09@gmail.com>
2015-07-31 14:55:51 +0400

    Switched staging branch to generated.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ set :domain, 'alpha.jumpstart.ge'
 set :user, 'prisoners-staging'
 set :application, 'Azeri-Prisoners-Staging'
 set :repository, 'git@github.com:JumpStartGeorgia/Azerbaijan-Political-Prisoners.git'
-set :repo_branch, 'master'
+set :repo_branch, 'generated'
 set :web_url, 'dev-prisoners.jumpstart.ge'
 set :use_ssl, true
 set :visible_to_robots, false

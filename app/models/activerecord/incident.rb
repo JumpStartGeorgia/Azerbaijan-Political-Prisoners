e0874e94ab79b1c39ae70bc69636e472a60fc338
nwshane <django09@gmail.com>
2015-08-11 17:33:26 +0400

    Refactored prisoner validations to work with separate incident form.

--- app/models/activerecord/incident.rb
+++ app/models/activerecord/incident.rb
@@ -7,17 +7,46 @@ class Incident < ActiveRecord::Base
   has_many :charges, dependent: :destroy
   has_many :articles, through: :charges
 
-  validates :date_of_arrest, presence: true
+  # Validations
+  validates :date_of_arrest, :prisoner, presence: true
+
+  def arrest_is_before_release
+    if date_of_release.present?
+      if date_of_arrest >= date_of_release
+        errors.add(:date_of_release, I18n.t('incident.errors.arrest_after_release'))
+      end
+    end
+  end
+  validate :arrest_is_before_release
+  private :arrest_is_before_release
+
+  def date_of_arrest_is_before_today
+    if date_of_arrest > Date.today
+      errors.add(:date_of_arrest, I18n.t('incident.errors.arrest_before_today'))
+    end
+  end
+  validate :date_of_arrest_is_before_today
+  private :date_of_arrest_is_before_today
+
+  def date_of_release_is_before_today
+    returns true if date_of_release.present? && date_of_release > Date.today
+    errors.add(:date_of_release, I18n.t('incident.errors.release_before_today'))
+  end
+  validate :date_of_release_is_before_today
+  private :date_of_release_is_before_today
+
+  def prisoner_incidents_have_valid_dates
+    prisoner.old_incidents_are_released
+    prisoner.incident_releases_before_arrests
+  end
+  validate :prisoner_incidents_have_valid_dates
+  private :prisoner_incidents_have_valid_dates
 
   # strip extra spaces before saving
   auto_strip_attributes :description_of_arrest, :description_of_release
 
   def released?
-    if date_of_release.nil?
-      return false
-    else
-      return true
-    end
+    return date_of_release.present?
   end
 
   def self.to_csv

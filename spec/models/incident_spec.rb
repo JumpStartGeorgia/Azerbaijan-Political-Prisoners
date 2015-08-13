584c747b827e73c9c5bdd1ab92037e95dcc26890
nwshane <django09@gmail.com>
2015-08-13 11:43:11 +0400

    Filled in specs for validations that compare new incident to already saved incident.

--- spec/models/incident_spec.rb
+++ spec/models/incident_spec.rb
@@ -69,33 +69,65 @@ RSpec.describe Incident, type: :model do
   end
 
   describe 'that is most recent incident on prisoner' do
-    it 'causes error if previous incident is not released' do
+    let(:previous_incident) do
+      FactoryGirl.create(
+        :incident,
+        date_of_arrest: incident1.date_of_arrest - 365,
+        date_of_release: incident1.date_of_arrest
 
+      )
+    end
+
+    it 'causes error if previous incident is not released' do
+      previous_incident.date_of_release = nil
+      expect(incident1).to have(1).error_on(:date_of_arrest)
     end
 
     describe 'with previous incident that is released' do
       it "causes error if previous incident's date of release is after date of arrest" do
-
+        previous_incident.date_of_release = incident1.date_of_arrest + 1
+        expect(incident1).to have(1).error_on(:date_of_arrest)
       end
 
       it "does not cause error if previous incident's date of release is before date of arrest" do
+        previous_incident.date_of_release = incident1.date_of_arrest - 1
+        expect(incident1).to be_valid
+      end
 
+      it "does not cause error if previous incident's date of release is same day as date of arrest" do
+        previous_incident.date_of_release = incident1.date_of_arrest
+        expect(incident1).to be_valid
       end
     end
   end
 
   describe 'that is not most recent incident on prisoner' do
-    it 'causes error if date of release is not present' do
+    let(:subsequent_incident) do
+      FactoryGirl.create(
+        :incident,
+        date_of_arrest: incident1.date_of_arrest + 365
+      )
+    end
 
+    it 'causes error if date of release is not present' do
+      incident1.date_of_release = nil
+      expect(incident1).to have(1).error_on(:date_of_release)
     end
 
     describe 'and with date of release' do
       it "causes error if date of release is after next incident's date of arrest" do
-
+        incident1.date_of_release = subsequent_incident.date_of_arrest + 1
+        expect(incident1).to have(1).error_on(:date_of_release)
       end
 
       it "does not cause error if date of release is before next incident's date of arrest" do
+        incident1.date_of_release = subsequent_incident.date_of_arrest - 1
+        expect(incident1).to be_valid
+      end
 
+      it "does not cause error if date of release is same day as next incident's date of arrest" do
+        incident1.date_of_release = subsequent_incident.date_of_arrest
+        expect(incident1).to be_valid
       end
     end
   end

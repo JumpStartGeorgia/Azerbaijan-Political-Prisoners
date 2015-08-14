58c2c1db9b31af8b8314d3d09a4c5838f11678ff
nwshane <django09@gmail.com>
2015-08-14 13:09:19 +0400

    Fixed incidents controller redirect paths.

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -6,17 +6,17 @@ class IncidentsController < ApplicationController
   before_action :set_tinymce_config_gon
   before_action :set_prisoner
 
-  # GET /incidents/new
+  # GET /prisoners/{@prisoner.slug}/incidents/new
   def new
     @incident = Incident.new
   end
 
-  # GET /incidents/1/edit
+  # GET /prisoners/{@prisoner.slug}/incidents/1/edit
   def edit
   end
 
-  # POST /incidents
-  # POST /incidents.json
+  # POST /prisoners/{@prisoner.slug}/incidents
+  # POST /prisoners/{@prisoner.slug}/incidents.json
   def create
     @incident = Incident.new(incident_params)
     @incident.prisoner_id = @prisoner.id
@@ -24,11 +24,11 @@ class IncidentsController < ApplicationController
     respond_to do |format|
       if @incident.save
         format.html do
-          redirect_to @incident,
+          redirect_to @prisoner,
                       notice: t('shared.msgs.success_created',
                                 obj: t('activerecord.models.incident', count: 1))
         end
-        format.json { render :show, status: :created, location: @incident }
+        format.json { render :show, status: :created, location: @prisoner }
       else
         format.html { render :new }
         format.json { render json: @incident.errors, status: :unprocessable_entity }
@@ -36,17 +36,17 @@ class IncidentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /incidents/1
-  # PATCH/PUT /incidents/1.json
+  # PATCH/PUT /prisoners/{@prisoner.slug}/incidents/1
+  # PATCH/PUT /prisoners/{@prisoner.slug}/incidents/1.json
   def update
     respond_to do |format|
       if @incident.update(incident_params)
         format.html do
-          redirect_to @incident,
+          redirect_to @prisoner,
                       notice: t('shared.msgs.success_updated',
                                 obj: t('activerecord.models.incident', count: 1))
         end
-        format.json { render :show, status: :ok, location: @incident }
+        format.json { render :show, status: :ok, location: @prisoner }
       else
         format.html { render :edit }
         format.json do
@@ -56,13 +56,13 @@ class IncidentsController < ApplicationController
     end
   end
 
-  # DELETE /incidents/1
-  # DELETE /incidents/1.json
+  # DELETE /prisoners/{@prisoner.slug}/incidents/1
+  # DELETE /prisoners/{@prisoner.slug}/incidents/1.json
   def destroy
     @incident.destroy
     respond_to do |format|
       format.html do
-        redirect_to prisoners_url,
+        redirect_to @prisoner,
                     notice: t('shared.msgs.success_destroyed',
                               obj: t('activerecord.models.incident', count: 1))
       end

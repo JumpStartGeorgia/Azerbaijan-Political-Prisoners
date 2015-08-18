48b062bc6c58a77a9197a0e6b84ddd2a14e7e4f4
nwshane <django09@gmail.com>
2015-08-18 14:37:29 +0400

    Using GeneratedFile clean filename wherever file is generated.

--- app/models/pw_csv.rb
+++ app/models/pw_csv.rb
@@ -1,53 +1,71 @@
 # Handles csv files
 # About name: PwCsv stands for Prisoners Watch CSV; avoids conflict with CSV gem
 class PwCsv
-  def self.getCsvFileName(model, timeStamp)
-    "#{model.model_name.plural}_#{timeStamp}.csv"
+  def self.plural_name(model)
+    model.model_name.plural
   end
-  private_class_method :getCsvFileName
+  private_class_method :plural_name
 
-  def self.createCsvZip(csv_zip, timeStamp)
+  def self.create_csv_files(csv_models, csv_dir)
+    csv_models.each do |csv_model|
+      new_csv_file_path = csv_dir.join(GeneratedFile.clean_filename(plural_name(csv_model), 'csv'))
+
+      File.open(new_csv_file_path, 'w') do |file|
+        file.write(csv_model.to_csv)
+      end
+    end
+  end
+  private_class_method :create_csv_files
+
+  def self.create_zip_from_csv_files(csv_zip, csv_models, csv_dir)
+    Zip::File.open(csv_zip, Zip::File::CREATE) do |zipfile|
+      csv_models.each do |csv_model|
+        csv_file_path = Dir.glob(csv_dir.join("#{plural_name(csv_model)}_*.csv"))[0]
+        csv_file_name = File.basename(csv_file_path)
+
+        zipfile.add(csv_file_name, csv_file_path)
+      end
+    end
+  end
+  private_class_method :create_zip_from_csv_files
+
+  def self.remove_csv_files(csv_models, csv_dir)
+    csv_models.each do |csv_model|
+      csv_file_path = Dir.glob(csv_dir.join("#{plural_name(csv_model)}_*.csv"))[0]
+
+      File.delete(csv_file_path)
+    end
+  end
+  private_class_method :remove_csv_files
+
+  def self.create_csv_zip(csv_zip)
     require 'zip'
 
     # Calls to_csv on each of these models and includes it in the zip
     csv_models = [Prison, Tag, Article, Prisoner, Incident, CriminalCode]
 
+    # Create the directory to contain the csv files
     csv_dir = Pathname.new(File.dirname(csv_zip))
     FileUtils.mkdir_p(csv_dir)
 
-    # Create csv files
-    csv_models.each do |csv_model|
-      File.open(csv_dir.join(getCsvFileName(csv_model, timeStamp)), 'w') { |file| file.write(csv_model.to_csv) }
-    end
-
-    # Create zip from csv files
-    Zip::File.open(csv_zip, Zip::File::CREATE) do |zipfile|
-      csv_models.each do |csv_model|
-        zipfile.add(getCsvFileName(csv_model, timeStamp), csv_dir.join(getCsvFileName(csv_model, timeStamp)))
-      end
-    end
-
-    # Delete csv files
-    csv_models.each do |csv_model|
-      File.delete(csv_dir.join(getCsvFileName(csv_model, timeStamp)))
-    end
+    create_csv_files(csv_models, csv_dir)
+    create_zip_from_csv_files(csv_zip, csv_models, csv_dir)
+    remove_csv_files(csv_models, csv_dir)
   end
-  private_class_method :createCsvZip
+  private_class_method :create_csv_zip
 
   def self.zip_all
     csv_zip = Dir.glob(Rails.root.join('public', 'generated', 'csv', I18n.locale.to_s, 'political_prisoner_data_*.zip'))[0]
 
     unless csv_zip
-      timeStamp = GeneratedFile.timeStamp
       csv_zip = Rails.root
         .join('public',
               'generated',
               'csv',
               I18n.locale.to_s,
-              GeneratedFile.clean_filename('political_prisoner_data', 
-                                           'zip',
-                                           timeStamp))
-      createCsvZip(csv_zip, timeStamp)
+              GeneratedFile.clean_filename('political_prisoner_data', 'zip'))
+
+      create_csv_zip(csv_zip)
     end
 
     csv_zip

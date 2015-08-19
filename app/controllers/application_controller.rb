9faa6a29e71a56101770ad6c94e4bca667a7aa49
nwshane <django09@gmail.com>
2015-08-19 13:28:57 +0400

    Added select2 az translation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
+    gon.locale = I18n.locale
   end
 
   def set_global_variables

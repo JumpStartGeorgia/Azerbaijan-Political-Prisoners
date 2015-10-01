40a92e47330ed66e0ebb6fd5f2607e14db80048c
nwshane <django09@gmail.com>
2015-10-01 11:39:02 +0400

    Changed capybara driver to chrome.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,6 +9,12 @@ require 'capybara-screenshot/rspec'
 # Only keeps html and png screenshots from the last test run
 Capybara::Screenshot.prune_strategy = :keep_last_run
 
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+
+Capybara.current_driver = :selenium_chrome
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in

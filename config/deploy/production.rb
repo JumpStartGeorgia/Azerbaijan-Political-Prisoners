4c852745e9ae297dbba392d7386d9d2d35a8201c
jason <jason.addie@gmail.com>
2016-12-02 14:35:22 +0400

    switching servers

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
-set :domain, 'prisoners.watch'
+set :domain, 'prisoners.jumpstart.ge'
 set :user, 'prisoners'
 set :application, 'Prisoners'
-set :repository, 'git@github.com:JumpStartGeorgia/Azerbaijan-Political-Prisoners.git'
+set :repository, 'https://github.com/JumpStartGeorgia/Azerbaijan-Political-Prisoners.git'
 set :repo_branch, 'master'
 set :web_url, 'prisoners.watch'
-set :use_ssl, true
+set :use_ssl, false
